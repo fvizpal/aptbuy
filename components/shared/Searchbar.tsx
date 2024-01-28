05fe998103533b5c51fd41a21e4cdf6c91734577
fvizpal <vishalp9966@gmail.com>
2024-01-28 20:32:18 +0530

    initial commit

---
+++ components/shared/Searchbar.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { FormEvent, useState } from "react";
+import { scrapeAndStore } from "@/lib/actions";
+
+const Searchbar = () => {
+  const [searchPrompt, setSearchPrompt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+
+    try {
+      setIsLoading(true);
+
+      await scrapeAndStore(searchPrompt);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <form
+      className="flex flex-wrap gap-4 mt-12"
+      onSubmit={handleSubmit}
+    >
+        <input 
+          type="text"
+          value={searchPrompt}
+          onChange={(e) => setSearchPrompt(e.target.value)}
+          placeholder="Enter product link"
+          className="searchbar-input"
+        />
+
+        <button 
+          type="submit" 
+          className="searchbar-btn"
+          disabled={searchPrompt === ''}
+        >
+          {isLoading ? 'Searching...' : 'Search'}
+        </button>
+    </form>
+  )
+}
+
+export default Searchbar
